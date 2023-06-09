0233fdced5367f40c481162b1482f0447af63804
Sawera Khurshid <111198333+sawera261khurshid@users.noreply.github.com>
2023-06-09 23:12:27 +0900

    Update maxpool.py

--- maxpool.py
+++ maxpool.py
@@ -63,7 +63,9 @@ class MaxPool2:
           for f2 in range(f):
             # If this pixel was the max value, copy the gradient to it.
             if im_region[i2, j2, f2] == amax[f2]:
-              d_L_d_input[i * 2 + i2, j * 2 + j2, f2] = d_L_d_out[i, j, f2]
+              d_L_d_input[i * 2 + i2, j * 2 + j2, f2] = np.max(d_L_d_out[i, j, f2])
+
+#              d_L_d_input[i * 2 + i2, j * 2 + j2, f2] = d_L_d_out[i, j, f2]
 
     return d_L_d_input
 
