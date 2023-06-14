27a7b286abe232a3fbecf42982b0340f148a41e8
Sawera Khurshid <111198333+sawera261khurshid@users.noreply.github.com>
2023-06-14 12:12:01 +0900

    Update conv_fp16.py

--- conv_fp16.py
+++ conv_fp16.py
@@ -12,8 +12,8 @@ class Conv3x3_1_to_n_padding:
   # A Convolution layer using 3x3 filters.
 
   def __init__(self, output=1, input=1):
-    num_filters = output.astype(np.float16)
-    in_ch = input.astype(np.float16)
+    num_filters = np.float16(output)
+    in_ch = np.float16(input)
 
     self.num_filters = num_filters
 
@@ -83,8 +83,8 @@ class Conv3x3_n_to_n_padding:
   # A Convolution layer using 3x3 filters.
 
   def __init__(self, output=1, input=1):
-    num_filters = output.astype(np.float16)
-    in_ch = input.astype(np.float16)
+    num_filters = np.float16(output)
+    in_ch = np.float16(input)
 
     self.num_filters = num_filters
 
